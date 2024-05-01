e3f434c63da5c085db0c14a9276931085c0d20b7
EmreShakir1 <124499937+EmreShakir1@users.noreply.github.com>
2024-05-02 00:46:25 +0300

    Nav menu updated

--- BlazorApplication/Program.cs
+++ BlazorApplication/Program.cs
@@ -1,5 +1,9 @@
 ﻿using BlazorApplication.Components;
+using Bussines_Layer.Models;
+using DataLayer.Common;
+using DataLayer.ModelsContext;
 using Microsoft.AspNetCore.Identity;
+using ServiceLayer;
 
 namespace BlazorApplication
 {
@@ -9,9 +13,39 @@ namespace BlazorApplication
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+            builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+            builder.Services.AddDbContext<RentACarDbContext>();
+
+            builder.Services.AddScoped<CarCategoryManager, CarCategoryManager>();
+            builder.Services.AddScoped<IDb<CarCategory, int>, CarCategoryContext>();
+            builder.Services.AddScoped<CarCategoryContext, CarCategoryContext>();
+
+            builder.Services.AddScoped<CarManager, CarManager>();
+            builder.Services.AddScoped<IDb<Car, int>, CarContext>();
+            builder.Services.AddScoped<CarContext, CarContext>();
+
+            builder.Services.AddScoped<ReviewManager, ReviewManager>();
+            builder.Services.AddScoped<IDb<Review, int>, ReviewContext>();
+            builder.Services.AddScoped<ReviewContext, ReviewContext>();
+
+            builder.Services.AddScoped<ReservationManager, ReservationManager>();
+            builder.Services.AddScoped<IDb<Reservation, int>, ReservationContext>();
+            builder.Services.AddScoped<ReservationContext, ReservationContext>();
+
+            builder.Services.AddScoped<CustomerManager, CustomerManager>();
+            builder.Services.AddScoped<IDb<Customer, int>, CustomerContext>();
+            builder.Services.AddScoped<CustomerContext, CustomerContext>();
+
+            builder.Services.AddScoped<LocationManager, LocationManager>();
+            builder.Services.AddScoped<IDb<Location, int>, LocationContext>();
+            builder.Services.AddScoped<LocationContext, LocationContext>();
+
 
             var app = builder.Build();
 
